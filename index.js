a03fcb7679c3c4e742b563a162306a79250ebb06
Ivan.Orlov <ivan@morrr.com>
2015-04-20 10:03:49 +0300

    Initialize fix

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var Events = {
 
 	// Bind an event to a `callback` function. Passing `"all"` will bind
 	// the callback to all events fired.
-	on : function(name, callback, context) {
-		if (!eventsApi(this, 'on', name, [ callback, context ]) || !callback)
+	on: function(name, callback, context) {
+		if (!eventsApi(this, 'on', name, [callback, context]) || !callback)
 			return this;
 		this._events || (this._events = {});
 		var events = this._events[name] || (this._events[name] = []);
 		events.push({
-			callback : callback,
-			context : context,
-			ctx : context || this
+			callback: callback,
+			context: context,
+			ctx: context || this
 		});
 		return this;
 	},
 
 	// Bind an event to only be triggered a single time. After the first time
 	// the callback is invoked, it will be removed.
-	once : function(name, callback, context) {
-		if (!eventsApi(this, 'once', name, [ callback, context ]) || !callback)
+	once: function(name, callback, context) {
+		if (!eventsApi(this, 'once', name, [callback, context]) || !callback)
 			return this;
 		var self = this;
 		var once = _.once(function() {
@@ -35,8 +35,8 @@ var Events = {
 	// callbacks with that function. If `callback` is null, removes all
 	// callbacks for the event. If `name` is null, removes all bound
 	// callbacks for all events.
-	off : function(name, callback, context) {
-		if (!this._events || !eventsApi(this, 'off', name, [ callback, context ]))
+	off: function(name, callback, context) {
+		if (!this._events || !eventsApi(this, 'off', name, [callback, context]))
 			return this;
 
 		// Remove all callbacks for all events.
@@ -45,7 +45,7 @@ var Events = {
 			return this;
 		}
 
-		var names = name ? [ name ] : _.keys(this._events);
+		var names = name ? [name] : _.keys(this._events);
 		for (var i = 0, length = names.length; i < length; i++) {
 			name = names[i];
 
@@ -64,8 +64,7 @@ var Events = {
 			var remaining = [];
 			for (var j = 0, k = events.length; j < k; j++) {
 				var event = events[j];
-				if (callback && callback !== event.callback && callback !== event.callback._callback || context
-						&& context !== event.context) {
+				if (callback && callback !== event.callback && callback !== event.callback._callback || context && context !== event.context) {
 					remaining.push(event);
 				}
 			}
@@ -84,7 +83,7 @@ var Events = {
 	// passed the same arguments as `trigger` is, apart from the event name
 	// (unless you're listening on `"all"`, which will cause your callback to
 	// receive the true name of the event as the first argument).
-	trigger : function(name) {
+	trigger: function(name) {
 		if (!this._events)
 			return this;
 		var args = arguments;
@@ -101,7 +100,7 @@ var Events = {
 
 	// Tell this object to stop listening to either specific events ... or
 	// to every object it's currently listening to.
-	stopListening : function(obj, name, callback) {
+	stopListening: function(obj, name, callback) {
 		var listeningTo = this._listeningTo;
 		if (!listeningTo)
 			return this;
@@ -110,7 +109,7 @@ var Events = {
 			callback = this;
 		if (obj)
 			(listeningTo = {})[obj._listenId] = obj;
-		for ( var id in listeningTo) {
+		for (var id in listeningTo) {
 			obj = listeningTo[id];
 			obj.off(name, callback, this);
 			if (remove || _.isEmpty(obj._events))
@@ -132,8 +131,8 @@ var eventsApi = function(obj, action, name, rest) {
 
 	// Handle event maps.
 	if (typeof name === 'object') {
-		for ( var key in name) {
-			obj[action].apply(obj, [ key, name[key] ].concat(rest));
+		for (var key in name) {
+			obj[action].apply(obj, [key, name[key]].concat(rest));
 		}
 		return false;
 	}
@@ -142,7 +141,7 @@ var eventsApi = function(obj, action, name, rest) {
 	if (eventSplitter.test(name)) {
 		var names = name.split(eventSplitter);
 		for (var i = 0, length = names.length; i < length; i++) {
-			obj[action].apply(obj, [ names[i] ].concat(rest));
+			obj[action].apply(obj, [names[i]].concat(rest));
 		}
 		return false;
 	}
@@ -154,34 +153,38 @@ var eventsApi = function(obj, action, name, rest) {
 // triggering events. Tries to keep the usual cases speedy (most internal
 // Backbone events have 3 arguments).
 var triggerEvents = function(events, args) {
-	var ev, i = -1, l = events.length, a1 = args[0], a2 = args[1], a3 = args[2];
+	var ev, i = -1,
+		l = events.length,
+		a1 = args[0],
+		a2 = args[1],
+		a3 = args[2];
 	switch (args.length) {
-	case 0:
-		while (++i < l)
-			(ev = events[i]).callback.call(ev.ctx);
-		return;
-	case 1:
-		while (++i < l)
-			(ev = events[i]).callback.call(ev.ctx, a1);
-		return;
-	case 2:
-		while (++i < l)
-			(ev = events[i]).callback.call(ev.ctx, a1, a2);
-		return;
-	case 3:
-		while (++i < l)
-			(ev = events[i]).callback.call(ev.ctx, a1, a2, a3);
-		return;
-	default:
-		while (++i < l)
-			(ev = events[i]).callback.apply(ev.ctx, args);
-		return;
+		case 0:
+			while (++i < l)
+				(ev = events[i]).callback.call(ev.ctx);
+			return;
+		case 1:
+			while (++i < l)
+				(ev = events[i]).callback.call(ev.ctx, a1);
+			return;
+		case 2:
+			while (++i < l)
+				(ev = events[i]).callback.call(ev.ctx, a1, a2);
+			return;
+		case 3:
+			while (++i < l)
+				(ev = events[i]).callback.call(ev.ctx, a1, a2, a3);
+			return;
+		default:
+			while (++i < l)
+				(ev = events[i]).callback.apply(ev.ctx, args);
+			return;
 	}
 };
 
 var listenMethods = {
-	listenTo : 'on',
-	listenToOnce : 'once'
+	listenTo: 'on',
+	listenToOnce: 'once'
 };
 
 // Inversion-of-control versions of `on` and `once`. Tell *this* object to
@@ -247,7 +250,9 @@ var extend = function(protoProps, staticProps) {
 };
 
 var Class = function(protoProps, staticProps) {
-	this.initialize.apply(this, arguments);
+	if (this.initialize) {
+		this.initialize.apply(this, arguments);
+	}
 };
 Class.extend = extend;
 
